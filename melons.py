3a34056a8016161fd398f72866ac0898d29edb2e
Stephanie Resis <sresis@umich.edu>
2020-07-17 19:31:53 +0000

    added part 3 functionality for govt orders

--- melons.py
+++ melons.py
@@ -14,14 +14,15 @@ class AbstractMelonOrder:
       
         base_price = 5
         
+        # if the species is Christmas, base price is 1.5x higher
         if self.species == 'Christmas':
             base_price = base_price * 1.5
         total = (1 + self.tax) * self.qty * base_price
         
+        # if it is international and < 10 melons, add $3 fee to the total
         if self.order_type == 'international' and self.qty < 10:
             total = total + 3
        
-        
 
         return total
 
@@ -29,6 +30,15 @@ class AbstractMelonOrder:
         """Record the fact than an order has been shipped."""
 
         self.shipped = True
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    def __init__(self, species, qty):
+        super().__init__(species, qty, "government", 0.00)
+        self.passed_inspection = False
+
+    def mark_inspection(self, passed):
+        self.passed_inspection = passed
+
 
 
 class DomesticMelonOrder(AbstractMelonOrder):
@@ -49,10 +59,6 @@ class InternationalMelonOrder(AbstractMelonOrder):
         """Initialize melon order attributes."""
         super().__init__(species, qty, "international", 0.17)
         self.country_code = country_code
-
-    
-     
- 
 
 
     def get_country_code(self):
